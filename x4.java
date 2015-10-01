622a21226cce17774d0a162b76fb7c349a9cb34f
Luis Fuentes <angel_5fuentes@outlook.com>
2015-10-01 16:10:32 -0700

    Update x4.java

--- x4.java
+++ x4.java
@@ -1,51 +1,29 @@
-//// Exercise 4:  click button to change background.
-
-int r,g,b;
-float button1X=100, button1Y=100, button1W=80, button1H=40;
-int counter=0;
-
-//// SETUP:  size only.  Also set colors.
-void setup() {
-  size( 640, 480 );
-  reset();
+void setup(){
+  size(800,800);
+  background(255);
+ 
 }
-void reset() {
-  r=  100;
-  g=  200;
-  b=  250;
+void draw(){
+ 
+  rectMode(CENTER);
+  fill(100,125,130);
+  rect(0,0,500,400);
+  fill(100,1,190);
+  textSize(30);
+  text("CLICK for white",0,100 );
+  text("CLICK outside for black",0,500 );
+ 
+    
 }
-
-
-//// NEXT:  button only.
-void draw() {
-  background( r,g,b );
-  showButton( button1X, button1Y, button1W, button1H );
-  fill(100,0,0);
-  text( "Click me!", button1X+button1W/4, button1Y+button1H*2/3 );
-}
-// Draw the button.
-void showButton( float x, float y, float w, float h ) {
-  fill( 255,255,0 );
-  rect ( x,y, w,h );
-}
-
-//// HANDLERS:  keys & click
-void keyPressed() {
-  if (key == 'q') exit();
-  if (key == 'r') reset();
-}
-void mousePressed() {
-  if ( hit( mouseX,mouseY, 100,100, 50,50 ) ) {
-    counter=  counter+1;
-    if (counter % 2 > 0) {
-      r=  255;
-      g=  50;
-      b=  0;
-    } else {
-      reset();
+  void mousePressed() {
+  if ( hit( mouseX,mouseY,100,100,100,100) ) {
+background(255);
+ } else {
+      background(0);
+ 
     }
   }
-}
+
 
 //// OTHER METHODS:  detect "hit"
 // Return true if "near"
@@ -61,3 +39,4 @@ boolean hit( float x1, float y1, float x2, float y2, float w, float h ) {
 
   return result;
 }
+
